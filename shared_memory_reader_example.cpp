f1cc75de909d832ef48c373b1fbb3c691f195597
andriiprysiazhnyk <prysiazhnyk@ucu.edu.ua>
2019-12-27 01:59:03 +0200

    Add shared memory usage examles. Modify cmake.

--- shared_memory_reader_example.cpp
+++ shared_memory_reader_example.cpp
@@ -2,3 +2,26 @@
 // Created by andriiprysiazhnyk on 12/27/19.
 //
 
+#include <iostream>
+#include "MySharedMemory.h"
+
+int main() {
+    size_t size = 4096;
+
+    MySharedMemory sharedMemory("shared_memory_example");
+    bool res = sharedMemory.attach(size);
+    if (!res) {
+        std::cout << sharedMemory.error_message;
+        return 1;
+    }
+
+    char *data = (char *)sharedMemory.data();
+
+    for(int counter = 0; counter < 100000; ++counter) {
+        sharedMemory.lock();
+        std::cout << data;
+        sharedMemory.unlock();
+    }
+
+    return 0;
+}
--- shared_memory_writer_example.cpp
+++ shared_memory_writer_example.cpp
@@ -2,3 +2,34 @@
 // Created by andriiprysiazhnyk on 12/27/19.
 //
 
+#include <iostream>
+#include <sstream>
+#include "MySharedMemory.h"
+
+int main() {
+    size_t size = 4096;
+
+    MySharedMemory sharedMemory("shared_memory_example");
+    bool res = sharedMemory.create(size);
+    if (!res) {
+        std::cout << sharedMemory.error_message;
+        return 1;
+    }
+
+    char *data = (char *)sharedMemory.data();
+
+    std::string message_start = "Message number: ";
+    for(int counter = 0;; ++counter) {
+        sharedMemory.lock();
+
+        std::stringstream ss;
+        ss << counter;
+
+        std::string message = message_start + ss.str() + "\n";
+        std::copy(&message[0], &message[0] + message.size() + 1, data);
+
+        sharedMemory.unlock();
+    }
+
+    return 0;
+}
